01a8af61d607d215678385fb9f77cd1b96683684
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-08-20 20:21:32 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -65,17 +65,18 @@ const StartSessionIntentHandler = {
             const timerId = timerResponse.id;
             const timerStatus = timerResponse.status;
             
-            const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-            sessionAttributes.startedText = "Wow, it is started";
-            
-            handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-            const speakOutput = 'Hello World! Start! I saved this attributes ${sessionAttributes.startedText}';
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-                .getResponse();
-            
+            if (timerStatus === 'ON') {
+                const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+                sessionAttributes['startedText'] = "Wow, it is started";
+                
+                handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+    
+                const speakOutput = 'Hello World! Start! I saved this attributes';
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+                    .getResponse();
+            }            
         } catch (error) {
             
         }
