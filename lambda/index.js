1c495b04aebcf7668d2b6551e60c316e4cb669eb
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-08-22 22:22:38 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -119,6 +119,7 @@ const StartSessionIntentHandler = {
                                     },
                                     {
                                         type: "SendEvent",
+                                        when: "${elapsedTime > 120000}"
                                         arguments: [
                                           "timeout clicked the button"
                                         ]
