93f54f2cd832c58386a78dfe821c1f6b708f8715
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-08-23 19:15:05 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -149,7 +149,7 @@ function renderPausedSessionAPLDocument(attributesManager, responseBuilder) {
     
     attributesManager['minutes'] = diff / 60;
     attributesManager['seconds'] = diff % 60;
-    sessionAttributes['durationMS'] -= (diff * 1000);
+    attributesManager['durationMS'] -= (diff * 1000);
     
     responseBuilder
     .addDirective({
