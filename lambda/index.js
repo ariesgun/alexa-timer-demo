08f7f96cb565dfbea32e1a115d42fb15d8a33b0d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-08-23 10:45:29 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -105,7 +105,7 @@ const StartSessionIntentHandler = {
                             sessionData: {
                                 title: "FOCUS",
                                 session: "focus",
-                                duration: 25
+                                duration: 2
                                 
                             }
                         }
